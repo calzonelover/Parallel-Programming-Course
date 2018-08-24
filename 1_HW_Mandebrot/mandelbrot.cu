488e941003c8f2f7c12e897faadebcb948171cb8
calzonelover <patomporn.pay@gmail.com>
2018-08-24 18:41:36 +0700

    need to run

--- 1_HW_Mandebrot/mandelbrot.cu
+++ 1_HW_Mandebrot/mandelbrot.cu
@@ -3,15 +3,23 @@
 
 #include "mandelbrot.h"
 
-__global__ void MandelFunc (float *Zx, float *Zy, float *K, int n_x, int n_y, float dx, float dy, float x_i, float y_i){
+__global__ void 
+MandelFunc (float *Zx, float *Zy, float *K
+,int n_x, int n_y, float dx, float dy
+, float x_i, float y_i, float x_f, float y_f, int iter_num)
+{
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
 	int j = blockDim.y * blockIdx.y + threadIdx.y;
 	if (i < n_x && j < n_y){
-		int indx = i + j*n_x;
-		float Zx_prev = Zx[indx];
-		float Zy_prev = Zy[indx];
-		Zx[indx] = Zx_prev*Zx_prev - Zy_prev*Zy_prev + (dx*i+x_i);
-		Zy[indx] = 2.0*Zx_prev*Zy_prev + (dy*j+y_i);
+		if ( Zx[indx]*Zx[indx] + Zy[indx]*Zy[indx] < (x_f - x_i + y_f - y_i)) {
+			int indx = i + j*n_x;
+			float Zx_prev = Zx[indx];
+			float Zy_prev = Zy[indx];
+			Zx[indx] = Zx_prev*Zx_prev - Zy_prev*Zy_prev + (dx*i+x_i);
+			Zy[indx] = 2.0*Zx_prev*Zy_prev + (dy*j+y_i);
+		} else {
+			K[indx] = iter_num%16;
+		}
 	}
 }
 
@@ -44,7 +52,7 @@ int main(int argc, char const *argv[])
 	// init value
 	for (unsigned int i=0; i<n_x; i++){
     	for (unsigned int j=0; j<n_y; j++){
-    		h_Zx[i+j*n_x] = h_Zy[i+j*n_y] = 0;
+    		h_Zx[i+j*n_x] = h_Zy[i+j*n_y] = h_k[i+j*n_y] = 0;
     	}
     }
     // move memory to device
@@ -55,17 +63,9 @@ int main(int argc, char const *argv[])
     printf("CUDA kernel launch with %dx%dx%d blocks of %dx%dx%d threads\n", B.x, B.y, B.z, G.x, G.y, G.z);
 
     for (unsigned int l=0; l < max_iter; l++){
-    	MandelFunc<<<G, B>>>(d_Zx, d_Zy, d_k, n_x, n_y, dx, dy, x_i, y_i);
+    	MandelFunc<<<G, B>>>(d_Zx, d_Zy, d_k, n_x, n_y, dx, dy, x_i, y_i, x_f, y_f, l);
     }
-    // save
-	for (unsigned int i=0; i<n_x; i++){
-    	for (unsigned int j=0; j<n_y; j++){
-    		if (h_k[i+j*n_x] < (x_f - x_i + y_f - y_i))
-    			h_k[i+j*n_x] = 0;
-    		else
-    			h_k[i+j*n_x] %= 16;
-    	}-
-    }
+    
     end = clock();
     printf("GPU time = %lf\n", (double) (end - start)/ CLOCKS_PER_SEC );
 
