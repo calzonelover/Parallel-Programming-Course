962086dd0b75eacfec2a0f5bdd28923745580d5e
physics <patomporn.pay@gmail.com>
2018-10-18 13:19:25 +0700

    fix

--- 9_Lec/matmul_masterSlave.c
+++ 9_Lec/matmul_masterSlave.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 #include <mpi.h>
+
+#define TAG_DONE 0
+#define TAG_INPROGRESS 1
+
 int main(int argc, char** argv) {
-  int n = 10, i, j, numsent = 0;
+  int n = 5, i, j, numsent = 0;
   int rank, size;
   MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   if (rank == 0) {
-    for (i=0; i<size-1; i++) {
+    // First send
+    for (i=1; i<size; i++) {
       j = numsent++;
-      MPI_Send(&j, 1, MPI_INT, i+1, 1, MPI_COMM_WORLD);
+      MPI_Send(&j, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
     }
+    // blocking receive and send until all done
+    // while (1){
+    //   MPI_Recv(&j, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    //   int slave = status.MPI_SOURCE;
+    //   printf("Master receive %d from slave %d\n", j, slave);
+    //   if (j < n){
+    //     j = numsent++;
+    //     MPI_Send(&j, 1, MPI_INT, slave, TAG_INPROGRESS, MPI_COMM_WORLD);
+    //   } else {
+    //     for (i=1; i<size; i++){
+    //       MPI_Send(&j, 1, MPI_INT, i, TAG_DONE, MPI_COMM_WORLD);
+    //     }
+    //     break;
+    //   }
+    // }
     for (i=0; i<n; i++) {
       MPI_Recv(&j, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
       int slave = status.MPI_SOURCE;
-      printf("receive %d from rank %d\n", j, slave);
+      printf("Master receive %d from slave %d\n", j, slave);
       if (numsent < n) {
         j = numsent++;
-        MPI_Send(&j, 1, MPI_INT, slave, 1, MPI_COMM_WORLD);
+        MPI_Send(&j, 1, MPI_INT, slave, TAG_INPROGRESS, MPI_COMM_WORLD);
       }
       else {
-        MPI_Send(&j, 1, MPI_INT, slave, 0, MPI_COMM_WORLD);
+        MPI_Send(&j, 1, MPI_INT, slave, TAG_DONE, MPI_COMM_WORLD);
       }
     }
   }
@@ -30,10 +50,10 @@ int main(int argc, char** argv) {
     do {
       MPI_Recv(&j, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
       tag = status.MPI_TAG;
-      printf("Rank %d: receive %d, tag %d\n", rank, j, tag);
-      if (tag == 0) break;
+      printf("Slave %d: receive %d, tag %d\n", rank, j, tag);
+      if (tag == TAG_DONE) break;
       MPI_Send(&j, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
-    } while (tag == 1);
+    } while (tag == TAG_INPROGRESS);
   }
   MPI_Finalize();
   return 0;
