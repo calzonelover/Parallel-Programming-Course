66a600cf878a15f2a0bf6414f48415b5242d3cbe
calzonelover <patomporn.pay@gmail.com>
2018-10-24 02:56:17 +0700

    fix graph

--- 9_HW_MatMul_MasterSlave/5_plot.py
+++ 9_HW_MatMul_MasterSlave/5_plot.py
@@ -8,9 +8,11 @@ t_s = np.genfromtxt("5_data.dat")[1:,2]
 t_p = np.genfromtxt("5_data.dat")[1:,3]
 s = np.divide(t_s,t_p)
 
+x = np.power(2, x)
 print(x, s)
 
 plt.plot(x, s)
-plt.xlabel("log2(Number Processes)")
+plt.title("Same problem size per process")
+plt.xlabel("Number Processes")
 plt.ylabel("Speed up factor")
 plt.show()
